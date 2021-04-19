c570552e11c9e821c9ab358e9b72d2198801cca8
FabiGamerRO <fabi2001231@yahoo.com>
2021-04-19 20:05:18 +0300

    DRINK-71

--- src/main/java/root/ControllerMenu.java
+++ src/main/java/root/ControllerMenu.java
@@ -6,15 +6,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import javax.validation.constraints.Null;
+
 public class ControllerMenu {
-    @FXML
-    private Button logout1, logout2;
 
     private Parent root;
-
+    @FXML
+    private Button logout1, logout2;
     public void logout(ActionEvent event) throws Exception {
+        FXMLLoader fxmlLoader2 = new FXMLLoader();
         Stage stage4 = new Stage();
         if(event.getSource() == logout1) {
             stage4 = (Stage) logout1.getScene().getWindow();
@@ -24,9 +30,9 @@ public class ControllerMenu {
             stage4 = (Stage) logout2.getScene().getWindow();
             root = FXMLLoader.load(getClass().getResource("/fxml/register.fxml"));
         }
-        Scene scene2 = new Scene(root);
-        stage4.setTitle("AllYouCanDrink");
-        stage4.setScene(scene2);
+        Scene scene4 = new Scene(root);
+        stage4.setScene(scene4);
         stage4.show();
     }
 }
+
