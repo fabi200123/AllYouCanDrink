5bf52e7c9b0163502f7924cf2041f0e898048291
Didi <dianavelciov@yahoo.com>
2021-05-11 01:10:14 +0300

    DRINK-29 create database for drinks and bars

--- src/main/java/root/GenericItemController.java
+++ src/main/java/root/GenericItemController.java
@@ -2,6 +2,10 @@ package root;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.dizitart.no2.Nitrite;
+import org.dizitart.no2.objects.ObjectRepository;
+
+import static root.MenuFileSystemService.getPathToFile;
 
 public class GenericItemController {
 
@@ -14,5 +18,15 @@ public class GenericItemController {
 
     public void setLabel(String content) {
         label.setText(content);
+    }
+
+    private static ObjectRepository<BarcoolList> userRepository2;
+
+    public static void initDatabaseForBarcool() {
+        Nitrite database = Nitrite.builder()
+                .filePath(getPathToFile("barcool.db").toFile())
+                .openOrCreate("admin", "admin");
+
+        userRepository2 = database.getRepository(BarcoolList.class);
     }
 }
--- src/main/java/root/MenuFileSystemService.java
+++ src/main/java/root/MenuFileSystemService.java
@@ -3,8 +3,8 @@ package root;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class FileSystemService {
-    private static final String APPLICATION_FOLDER = ".registration";
+public class MenuFileSystemService {
+    private static final String APPLICATION_FOLDER = ".barcool";
     private static final String USER_FOLDER = System.getProperty("user.home");
     public static final Path APPLICATION_HOME_PATH = Paths.get(USER_FOLDER, APPLICATION_FOLDER);
 
