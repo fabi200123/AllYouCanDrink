ddcc400b54d4731d2d11c6132e3003af672578e4
Didi <dianavelciov@yahoo.com>
2021-05-13 01:28:48 +0300

    DRINK-43 DRINK-44 DRINK-47 DRINK-49 implement the functionality for the like and dislike buttons

--- src/main/java/root/BarcoolList.java
+++ src/main/java/root/BarcoolList.java
@@ -17,11 +17,31 @@ public class BarcoolList {
     private String name;
     private String details;
     private String category;
+    private int like;
+    private int dislike;
 
     public BarcoolList(String name, String details, String category) {
         this.name = name;
         this.details = details;
         this.category = category;
+        this.like = 0;
+        this.dislike = 0;
+    }
+
+    public BarcoolList(String name, String details, String category, int like, int dislike) {
+        this.name = name;
+        this.details = details;
+        this.category = category;
+        this.like = like;
+        this.dislike = dislike;
+    }
+
+    public BarcoolList(BarcoolList barcool) {
+        this.name = barcool.getName();
+        this.details = barcool.getDetails();
+        this.category = barcool.getCategory();
+        this.like = barcool.getLike();
+        this.dislike = barcool.getDislike();
     }
 
     public BarcoolList() {
@@ -29,6 +49,10 @@ public class BarcoolList {
 
     public static void addBarcool(String name, String details, String category){
         root.GenericItemController.userRepository2.insert(new BarcoolList(name, details, category));
+    }
+
+    public static void addBarcoolElement(BarcoolList element){
+        root.GenericItemController.userRepository2.insert(new BarcoolList(element));
     }
 
     public String getName() {
@@ -51,5 +75,13 @@ public class BarcoolList {
     public void setCategory(String category) {
             this.category = category;
     }
+
+    public int getLike() {
+        return like;
+    }
+    public void setLike() { like = like + 1; }
+
+    public int getDislike() { return dislike; }
+    public void setDislike() { dislike = dislike + 1; }
 
 }
--- src/main/java/root/GenericItemController.java
+++ src/main/java/root/GenericItemController.java
@@ -1,17 +1,24 @@
 package root;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
 import org.dizitart.no2.Nitrite;
 import org.dizitart.no2.NitriteCollection;
 import org.dizitart.no2.objects.ObjectRepository;
 
+import static org.dizitart.no2.objects.filters.ObjectFilters.eq;
 import static root.MenuFileSystemService.getPathToFile;
 
 public class GenericItemController {
 
     @FXML
     private Label label;
+    @FXML
+    private Button likebutton, dislikebutton;
+
+    private int flag = 0;
 
     public Label getLabel() {
         return label;
@@ -35,5 +42,43 @@ public class GenericItemController {
 
     public static void closeDatabaseForBarcool(){
         database.close();
+    }
+
+    public void handleLike(){
+        if(flag == 0) {
+            likebutton.setStyle("-fx-background-color: LightGreen; ");
+            flag = 1;
+        }
+        String s = label.getText();
+        String ss[]= s.split("\n");
+        s = ss[0];
+        BarcoolList aux = null;
+        for(BarcoolList t : userRepository2.find()){
+            if(t.getName().equals(s)){
+                aux = new BarcoolList(t.getName(), t.getDetails(), t.getCategory(), t.getLike() + 1, t.getDislike());
+                userRepository2.remove(eq("name", aux.getName()));
+                break;
+            }
+        }
+        BarcoolList.addBarcoolElement(aux);
+    }
+
+    public void handleDislike(){
+        if(flag == 0) {
+            dislikebutton.setStyle("-fx-background-color: #f51e1e; ");
+            flag = 1;
+        }
+        String s = label.getText();
+        String ss[]= s.split("\n");
+        s = ss[0];
+        BarcoolList aux = null;
+        for(BarcoolList t : userRepository2.find()){
+            if(t.getName().equals(s)){
+                aux = new BarcoolList(t.getName(), t.getDetails(), t.getCategory(), t.getLike(), t.getDislike() + 1);
+                userRepository2.remove(eq("name", aux.getName()));
+                break;
+            }
+        }
+        BarcoolList.addBarcoolElement(aux);
     }
 }
